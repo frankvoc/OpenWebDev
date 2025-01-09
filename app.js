45cd420a84c18eed385c87d36afcc7c2fdd8c092
frankvoc <fmvocatura@email.neit.edu>
2025-01-08 20:42:20 -0500

    init for open source web dev

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let message = "PEPPER!!!!";
+
+function sayPepper(){
+    console.log(message);
+}
+
+//sayPepper()
+
+//Route
+app.get("/",(req, res)=>{
+    res.sendFile(path.join(__dirname,"public","index.html"))
+});
+app.get("/json",(req, res)=>{
+    res.sendFile(path.join(__dirname,"public","players.json"))
+});
+
+//Creating listener on port 3000
+app.listen(PORT,()=>{
+    console.log("Server Running on port", PORT)
+})
