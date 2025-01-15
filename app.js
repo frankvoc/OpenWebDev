a021312180d812b7e5e35a32f2f17e178369d595
frankvoc <fmvocatura@email.neit.edu>
2025-01-15 17:37:02 -0500

    Node pathing and JSON display

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ app.use(express.static(path.join(__dirname,"public")));
 
 //Route
 app.get("/",(req, res)=>{
-    res.sendFile(path.join(__dirname,"public","index.html"))
+    res.redirect("/index");
 });
 app.get("/json",(req, res)=>{
     res.sendFile(path.join(__dirname,"public","players.json"))
 });
-
+//route for todo.json
+app.get("/todo",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","todo.json"));
+})
+//route for index
+app.get("/index",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","index2.html"));
+})
+//route for read-todo
+app.get("/read-todo", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "read-todo.html"));
+});
 //Creating listener on port 3000
 app.listen(PORT,()=>{
     console.log("Server Running on port", PORT)
